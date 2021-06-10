64a134c22479777483c306259ff1ec5bb426702b
alamgir8 <ahossain38900@gmail.com>
2021-06-10 18:51:47 +0600

    updated some desgin

--- src/Components/ImageSearch.js
+++ src/Components/ImageSearch.js
@@ -10,11 +10,11 @@ const ImageSearch = ({searchText}) => {
     return (
         <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
             <form onSubmit={onSubmit} className="w-full max-w-sm">
-                <div className="flex items-center border-b border-b-2 border-teal-500 border py-2">
+                <div className="flex items-center border-b border-b-2 border-teal-500 border py-2 rounded">
                     <input onChange={(e) => setText(e.target.value)} type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 
                     mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder='Search Your Image'/>
-                    <button type='submit' className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 
-                    border-teal-500 hover:border-teal-700 text-sm border-4 py-1 px-2 rounded">
+                    <button type='submit' className="flex-shrink-0 focus:outline-none bg-green-500 hover:bg-green-700 
+                    bg-green-500 hover:bg-green-700 text-white text-lg py-1 px-2 rounded">
                         Search
                     </button>
                 </div>
